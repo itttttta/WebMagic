b290f12320d81788a28578b881d10f48265e315b
wangda <wangda@zhuojianchina.com>
2017-01-03 17:47:41 +0800

    全站搜索

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/92/5081691654d10016138583d6b1eed3e7
@@ -0,0 +1,9 @@
+package com.wd.CoilingDragon.Dao;
+
+import com.wd.CoilingDragon.Enity.Book;
+
+public interface BookMapper {
+    int insert(Book record);
+
+    int insertSelective(Book record);
+}
--- WuXiaWorld/src/main/java/com/wd/CoilingDragon/WebMagic/BookWebMagic.java
+++ WuXiaWorld/src/main/java/com/wd/CoilingDragon/WebMagic/BookWebMagic.java
@@ -1,8 +1,16 @@
 package com.wd.CoilingDragon.WebMagic;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.wd.CoilingDragon.Dao.BookMapper;
+import com.wd.CoilingDragon.Dao.ContentMapper;
+import com.wd.CoilingDragon.Enity.Book;
 import com.wd.Tools.CoillingDragonTool;
+import com.wd.Tools.DBTools;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -23,14 +31,74 @@ public class BookWebMagic implements PageProcessor{
     
 	public void process(Page page) {
 		System.out.println("start");
+		
+			//href 正则
+		   final String hrefRegex = "(?<=href=\").*?(?=\")";
+    	   final Pattern hrefPattern = Pattern.compile(hrefRegex);
+    	   //title 正则
+    	   final String titleRegex = "(?<=\">).*?(?=<\\/a>)";
+    	   final Pattern titlePattern = Pattern.compile(titleRegex);
+
+    	   //数据库
+    	   SqlSession session = DBTools.getSession();
+    	   BookMapper mapper = session.getMapper(BookMapper.class);
+    		
 		// TODO Auto-generated method stub
 	     List<String> complectedBookList = page.getHtml().xpath("li[@id='menu-item-12207']/ul[@class='sub-menu']/li/a").all();
 	      for (int i = 0; i < complectedBookList.size(); i++) {
-	    	   System.out.println(i+"compected:  "+complectedBookList.get(i));
+	    	  
+	    	  //匹配href
+	    	   Book book = new Book();
+	    	   final Matcher hrefMatcher = hrefPattern.matcher(complectedBookList.get(i));
+	    	   while (hrefMatcher.find()) {
+	    		    System.out.println("href: " + hrefMatcher.group(0));
+	 	    	   book.setUrl(hrefMatcher.group(0));
+	    		}
+	    	   //匹配title
+	    	   final Matcher titleMatcher = titlePattern.matcher(complectedBookList.get(i));
+	    	   while (titleMatcher.find()) {
+	    		    System.out.println("title: " + titleMatcher.group(0));
+		 	    	   book.setBookName(titleMatcher.group(0));
+	    	   }
+	    	   book.setIsComplected(1);
+	    	   try {
+	      	        mapper.insert(book);
+	   	            System.out.println("成功插入 :"+book.toString());
+	      	        session.commit();
+	      	        CatalogueWebMagic catalogueWebMagic = new CatalogueWebMagic();
+	      	        catalogueWebMagic.setBook(book);
+	      	        catalogueWebMagic.run();
+	              } catch (Exception e) {
+	      	        e.printStackTrace();
+	      	        session.rollback();
+	              }
+	    	
+	    	   
 		}
 	      List<String> uncomplectedBookList = page.getHtml().xpath("li[@id='menu-item-2165']/ul[@class='sub-menu']/li/a").all();
 	      for (int i = 0; i < uncomplectedBookList.size(); i++) {
-	    	   System.out.println(i+"uncompected"+uncomplectedBookList.get(i));
+	    	  //匹配href
+//	    	   final Matcher hrefMatcher = hrefPattern.matcher(uncomplectedBookList.get(i));
+//	    	   while (hrefMatcher.find()) {
+//	    		    System.out.println("href: " + hrefMatcher.group(0));
+//	    		}
+//	    	   //匹配title
+//	    	   final Matcher titleMatcher = titlePattern.matcher(uncomplectedBookList.get(i));
+//	    	   while (titleMatcher.find()) {
+//	    		    System.out.println("title: " + titleMatcher.group(0));
+//	    	   }
+//	    	   Book book = new Book();
+//	    	   book.setBookName(titleMatcher.group(0));
+//	    	   book.setUrl(hrefMatcher.group(0));
+//	    	   book.setIsComplected( 1);
+//	    	   try {
+//	      	        mapper.insert(book);
+//	   	            System.out.println("成功插入 :"+book.toString());
+//	      	        session.commit();
+//	              } catch (Exception e) {
+//	      	        e.printStackTrace();
+//	      	        session.rollback();
+//	              }
 		}
 	     System.out.println("over");
 	}
